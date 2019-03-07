2c518a546d82f2dbc8d174c208b1c88c7140bca9
Philippine de Mauléon <philou@philippines-macbook-pro.home>
2019-03-07 12:17:55 +0100

    try2 withou the comment in upload

--- app/routes.py
+++ app/routes.py
@@ -53,6 +53,7 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
+        user.init_profile_pic()
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
