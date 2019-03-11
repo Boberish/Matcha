a9914e77fd52f25b368457e1245f9bb5942e552d
boberish <boberish@gmail.com>
2019-03-11 12:28:15 +0100

    added looks stuff

--- app/models.py
+++ app/models.py
@@ -12,6 +12,9 @@ import os
 likes = db.Table('likes',db.Column('likes_id', db.Integer, db.ForeignKey('user.id')), 
     db.Column('liked_id', db.Integer, db.ForeignKey('user.id')) )
 
+looks = db.Table('looks',db.Column('looks_id', db.Integer, db.ForeignKey('user.id')), 
+    db.Column('looked_id', db.Integer, db.ForeignKey('user.id')) )
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -23,13 +26,30 @@ class User(UserMixin, db.Model):
     fame = db.Column(db.Integer, index=True)
     bio = db.Column(db.String(256), index=True)
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    fame = db.Column(db.Integer, index=True)
     liked = db.relationship(
         'User', secondary=likes,
         primaryjoin=(likes.c.likes_id == id),
         secondaryjoin=(likes.c.liked_id == id),
-        backref=db.backref('likes', lazy='dynamic'),lazy='dynamic'
+        backref=db.backref('likes', lazy='dynamic'),lazy='dynamic')
+    looked = db.relationship(
+        'User', secondary=looks,
+        primaryjoin=(looks.c.looks_id == id),
+        secondaryjoin=(looks.c.looked_id == id),
+        backref=db.backref('looks', lazy='dynamic'),lazy='dynamic'
     )
 
+    def add_look(self, user):
+        if self.username != user.username:
+            self.looked.append(user)
+            db.session.commit()
+
+    def get_your_looks(self):
+        return User.query.join(looks, (looks.c.looked_id == User.id)).filter(looks.c.looks_id == self.id).all()
+
+    def get_looked_at_you(self):
+        return User.query.join(looks, (looks.c.looks_id == User.id)).filter(looks.c.looked_id == self.id).all()
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -57,6 +77,12 @@ class User(UserMixin, db.Model):
             return(os.path.join(os.getenv('PATH_IMAGE', '/static/images/'),'default_prof_pic', 'profile_empty.jpeg'))
         return(path + pic[0])
     
+    def get_user_img_paths(self):
+        filelist = os.listdir(os.path.join(app.config['UPLOAD_FOLDER'] + self.username))
+        final = [app.config['PATH_IMAGE']  + self.username + '/' + file for file in filelist if file != "profile_pic"]
+        return final
+
+
     def get_liked(self):
         print( '<User likes {}>'.format(self.liked.all()))
 
